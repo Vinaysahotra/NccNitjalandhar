7988bebeeb9981daf9f4aad020f5e3839084ed97
Vinaysahotra <sahotravinay9022gmail.com>
2020-12-27 12:12:57 +0530

    fixed bugs..

--- app/src/main/java/com/example/nccnitjalandhar/chats.java
+++ app/src/main/java/com/example/nccnitjalandhar/chats.java
@@ -35,26 +35,27 @@ import java.util.List;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class chats extends AppCompatActivity {
-CircleImageView profilephoto;
-TextView username;
-FirebaseUser fuser;
-Intent intent;
-ValueEventListener seenlistener;
-ImageButton send;
-RecyclerView chats;
-messageAdapter msgAdapter;
-List<allmessages>mchat;
-EditText messagesent;
-TextView onlinestatus;
+    CircleImageView profilephoto;
+    TextView username;
+    FirebaseUser fuser;
+    Intent intent;
+    ValueEventListener seenlistener;
+    ImageButton send;
+    RecyclerView chats;
+    messageAdapter msgAdapter;
+    List<allmessages> mchat;
+    EditText messagesent;
+    TextView onlinestatus;
 
-DatabaseReference reference;
+    DatabaseReference reference;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
-        Toolbar toolbar=findViewById(R.id.toolbar2);
-        onlinestatus=findViewById(R.id.online_status);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        onlinestatus = findViewById(R.id.online_status);
 
         setActionBar(toolbar);
         getActionBar().setTitle("");
@@ -62,84 +63,84 @@ DatabaseReference reference;
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(com.example.nccnitjalandhar.chats.this,contacts.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(new Intent(com.example.nccnitjalandhar.chats.this, contacts.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
-        fuser= FirebaseAuth.getInstance().getCurrentUser();
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
 
-profilephoto=findViewById(R.id.toolbarimage);
-username=findViewById(R.id.toolbarname);
-messagesent=findViewById(R.id.message);
+        profilephoto = findViewById(R.id.toolbarimage);
+        username = findViewById(R.id.toolbarname);
+        messagesent = findViewById(R.id.message);
 
-send=findViewById(R.id.send);
+        send = findViewById(R.id.send);
 
 
-        chats=findViewById(R.id.chatsrecycle);
+        chats = findViewById(R.id.chatsrecycle);
         chats.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         linearLayoutManager.setStackFromEnd(true);
         chats.setLayoutManager(linearLayoutManager);
 
 
-intent=getIntent();
-final String id=intent.getStringExtra("id");
+        intent = getIntent();
+        final String id = intent.getStringExtra("id");
 
-send.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        String message=messagesent.getText().toString();
-        if(!message.equals("")){
-            sendmessage(fuser.getUid(),id,message);
-        }
-        else{
-            Toast.makeText(chats.this,"can't send empty message",Toast.LENGTH_SHORT).show();
-        }
-        messagesent.setText("");
-    }
-});
-        reference= FirebaseDatabase.getInstance().getReference("users").child(id);
-reference.addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(@NonNull DataSnapshot snapshot) {
-       userinfo user=snapshot.getValue(userinfo.class);
-        assert user != null;
-        username.setText(user.getUsername());
-
-
-        if(user.getStatus().equals("online")){
-            onlinestatus.setText("online");
-        }
-        else{
-            onlinestatus.setText("offline");
-        }
-       if(!user.getProfilepic().equals("default")){
-           Glide.with(getApplicationContext()).load(user.getProfilepic()).into(profilephoto);
-       }
-       readMessages(fuser.getUid(),id,username.toString());
-    }
-
-    @Override
-    public void onCancelled(@NonNull DatabaseError error) {
-
-    }
-});
-seenMessage(id);
-
-
-    }
-    private void seenMessage(final String userId){
-        reference=FirebaseDatabase.getInstance().getReference("chats");
-        seenlistener=reference.addValueEventListener(new ValueEventListener() {
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = messagesent.getText().toString();
+                if (!message.equals("")) {
+                    sendmessage(fuser.getUid(), id, message);
+                } else {
+                    Toast.makeText(chats.this, "can't send empty message", Toast.LENGTH_SHORT).show();
+                }
+                messagesent.setText("");
+            }
+        });
+        reference = FirebaseDatabase.getInstance().getReference("users").child(id);
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
-                    allmessages message=dataSnapshot.getValue(allmessages.class);
-if(message.getReciever().equals(fuser.getUid())&& message.getSender().equals(userId)) {
-    HashMap<String, Object> map = new HashMap<>();
-    map.put("seen", true);
-    dataSnapshot.getRef().updateChildren(map);
+                userinfo user = snapshot.getValue(userinfo.class);
+                assert user != null;
+                username.setText(user.getUsername());
 
-}
+
+                if (user.getStatus().equals("online")) {
+                    onlinestatus.setText("online");
+                } else {
+                    onlinestatus.setText("offline");
+                }
+                if (!user.getProfilepic().equals("default")) {
+                    Glide.with(getApplicationContext()).load(user.getProfilepic()).into(profilephoto);
+                }
+                readMessages(fuser.getUid(), id, username.toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        seenMessage(id);
+
+    }
+
+    private void seenMessage(final String userId) {
+        reference = FirebaseDatabase.getInstance().getReference("chats");
+        seenlistener = reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    allmessages message = dataSnapshot.getValue(allmessages.class);
+                    assert message != null;
+                    if (message.getReciever().equals(fuser.getUid()) && message.getSender().equals(userId)) {
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("seen", true);
+                        dataSnapshot.getRef().updateChildren(map);
+
+                    }
                 }
             }
 
@@ -150,33 +151,36 @@ if(message.getReciever().equals(fuser.getUid())&& message.getSender().equals(use
         });
 
     }
-    private void sendmessage(String sender,String reciever,String message){
-        DatabaseReference reference=FirebaseDatabase.getInstance().getReference();
-        HashMap<String,Object>map=new HashMap<>();
-        map.put("sender",sender);
-        map.put("reciever",reciever);
-        map.put("message",message);
-        map.put("seen",false);
+
+    private void sendmessage(String sender, String reciever, String message) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("sender", sender);
+        map.put("reciever", reciever);
+        map.put("message", message);
+        map.put("seen", false);
         reference.child("chats").push().setValue(map);
     }
-    private  void readMessages(final String  myId, final String userId, final String username){
-        mchat=new ArrayList<>();
-        reference=FirebaseDatabase.getInstance().getReference("chats");
-        DatabaseReference ref2= FirebaseDatabase.getInstance().getReference("users").child(userId);
+
+    private void readMessages(final String myId, final String userId, final String username) {
+        mchat = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("chats");
+        DatabaseReference ref2 = FirebaseDatabase.getInstance().getReference("users").child(userId);
 
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 mchat.clear();
-                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
-                    allmessages chat=dataSnapshot.getValue(allmessages.class);
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    allmessages chat = dataSnapshot.getValue(allmessages.class);
                     assert chat != null;
-                    if(chat.getReciever().equals(myId)&&chat.getSender().equals(userId)||
-                        chat.getReciever().equals(userId)&&chat.getSender().equals(myId)){
-            mchat.add(chat);
+                    if ((chat.getReciever().equals(myId) && chat.getSender().equals(userId) )||
+                            ( chat.getReciever().equals(userId) && chat.getSender().equals(myId)) ){
+                        mchat.add(chat);
+
                     }
-                    msgAdapter=new messageAdapter(chats.this,mchat,username);
+                    msgAdapter = new messageAdapter(chats.this, mchat, username);
                     chats.setAdapter(msgAdapter);
                 }
 
@@ -188,12 +192,13 @@ if(message.getReciever().equals(fuser.getUid())&& message.getSender().equals(use
 
             }
         });
+
     }
 
-    private void status(String status){
-        reference=FirebaseDatabase.getInstance().getReference("users").child(fuser.getUid());
-        HashMap<String,Object>map=new HashMap<>();
-        map.put("status",status);
+    private void status(String status) {
+        reference = FirebaseDatabase.getInstance().getReference("users").child(fuser.getUid());
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("status", status);
         reference.updateChildren(map);
 
     }
@@ -209,11 +214,10 @@ if(message.getReciever().equals(fuser.getUid())&& message.getSender().equals(use
     @Override
     protected void onPause() {
         super.onPause();
-
-        status("offline");
         reference.removeEventListener(seenlistener);
-    }
+        status("offline");
 
+    }
 
 
 }
